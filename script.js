a33e1624f99b6cc341d244ad2daf2bcbef1a8732
Lucas Rossi <lucas.lrt1992@gmail.com>
2024-03-25 21:25:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(jogador1, jogador2){      
+    if (jogador1 == jogador2){
+        return "Empate!";
+    } else if ((jogador1 === "pedra" && jogador2 === "tesoura")||
+              (jogador1 === "papel" && jogador2 === "pedra")||
+              (jogador1 === "tesoura" && jogador2 === "papel"))        
+    {
+        return "Jogador 1 Venceu!";
+    } else {
+        return "Jogador 2 Venceu!"
+    }
+}
