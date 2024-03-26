31a8653465c644b4aaa24b7d3ccf38e052ee96a7
Lucas Rossi <lucas.lrt1992@gmail.com>
2024-03-25 21:34:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /* Desenvolva seu código abaixo */
-function playRockPaperScissor(jogador1, jogador2){      
-    if (jogador1 == jogador2){
+function playRockPaperScissor(jogador1, jogador2) {
+    
+    if (jogador1 === jogador2) {
         return "Empate!";
-    } else if ((jogador1 === "pedra" && jogador2 === "tesoura")||
-              (jogador1 === "papel" && jogador2 === "pedra")||
-              (jogador1 === "tesoura" && jogador2 === "papel"))        
-    {
-        return "Jogador 1 Venceu!";
+    } else if (
+        (jogador1 === "pedra" && jogador2 === "tesoura") ||
+        (jogador1 === "papel" && jogador2 === "pedra") ||
+        (jogador1 === "tesoura" && jogador2 === "papel")
+    ) {
+        return "Jogador 1 venceu!";
     } else {
-        return "Jogador 2 Venceu!"
+        return "Jogador 2 venceu!";
     }
 }
